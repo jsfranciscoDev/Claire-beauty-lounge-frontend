4bac30553cf5867a73cf7464ad56ec6e99a36f7f
ReysonDelameda <reysonjani.delameda@gmail.com>
2023-10-27 22:12:14 +0800

    frontend

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,50 +1,70 @@
 <script setup>
-import footerSection from '../components/footer.vue';
-import navBar from '../components/nav.vue'
+import footerSection from "../components/footer.vue";
+import navBar from "../components/nav.vue";
 </script>
- 
+
 <template>
-  <navBar/>
-  
-  <section id="home-section" class="hero" style="background-image: url(images/natolord.jpg);" data-stellar-background-ratio="0.5">
-		  <div class="home-slider owl-carousel">
-	      <div class="slider-item js-fullheight">
-	      	<div class="overlay"></div>
-	        <div class="container-fluid p-0">
-			      <div class="row d-md-flex no-gutters slider-text align-items-end justify-content-end" data-scrollax-parent="true">
-	          	<img class="one-third align-self-end order-md-last img-fluid" src="../assets/images/bg_2.png" alt="">
-		          <div class="one-forth d-flex align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
-		          	<div class="text mt-5">
-		          		<span class="subheading">Beauty Parlour</span>
-			            <h1 class="mb-4">Get Pretty Look</h1>
-			            <p class="mb-4">We provide huge facilities with advanced technology equipments and best quality service. Here we offer best treatment that you might have never experienced before.</p>
-		            </div>
-		          </div>
-	        	</div>
-	        </div>
-	      </div>
-	    </div>
-    </section>
+  <navBar />
 
-    <section>
-	
-		<div class="row">
-			<div class="col-sm-12 col-md-6  col-lg-6 column-two">
-				
-			</div>
-			<div class="col-sm-12 col-md-6  col-lg-6 column-one" >
-				<h2>We provide huge facilities with advanced technology equipments and best quality service. </h2>
-				<p class="mt-2"> Here we offer best treatment that you might have never experienced before.</p>
-				<button class="mt-3" ><router-link to="/book" class="text-dark">Book Now</router-link></button>
-			</div>
-			
-		</div>
-		
-    </section>
+  <section
+    id="home-section"
+    class="hero"
+    style="background-image: url(images/natolord.jpg)"
+    data-stellar-background-ratio="0.5"
+  >
+    <div class="home-slider owl-carousel">
+      <div class="slider-item js-fullheight">
+        <div class="overlay"></div>
+        <div class="container-fluid p-0">
+          <div
+            class="row d-md-flex no-gutters slider-text align-items-end justify-content-end"
+            data-scrollax-parent="true"
+          >
+            <img
+              class="one-third align-self-end order-md-last img-fluid"
+              src="../assets/images/bg_2.png"
+              alt=""
+            />
+            <div
+              class="one-forth d-flex align-items-center ftco-animate"
+              data-scrollax=" properties: { translateY: '70%' }"
+            >
+              <div class="text mt-5">
+                <span class="subheading">Claire Lounge</span>
+                <h1 class="mb-4">Get Pretty Look</h1>
+                <p class="mb-4">
+                  We provide huge facilities with advanced technology equipments
+                  and best quality service. Here we offer best treatment that
+                  you might have never experienced before.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
-    <footerSection></footerSection>
+  <section>
+    <div class="row">
+      <div class="col-sm-12 col-md-6 col-lg-6 column-two"></div>
+      <div class="col-sm-12 col-md-6 col-lg-6 column-one">
+        <h2>
+          We provide huge facilities with advanced technology equipments and
+          best quality service.
+        </h2>
+        <p class="mt-2">
+          Here we offer best treatment that you might have never experienced
+          before.
+        </p>
+        <button class="mt-3">
+          <router-link to="/book" class="text-dark">Book Now</router-link>
+        </button>
+      </div>
+    </div>
+  </section>
+
+  <footerSection></footerSection>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,38 +1,62 @@
 <template>
-    <footer class="ftco-footer ftco-section">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2>Claire Beauty Lounge</h2>
-              <p> Our main focus is on quality and hygiene. Our Parlour is well equipped with advanced technology equipments and provides best quality services.</p>           
+  <footer class="ftco-footer ftco-section">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md">
+          <div class="ftco-footer-widget mb-4">
+            <h2>Official Accounts:</h2>
+            <!-- <p> Our main focus is on quality and hygiene. Our Parlour is well equipped with advanced technology equipments and provides best quality services.</p>            -->
+            <div class="block-23 mb-3">
+              <ul>
+                <li>
+                  <span class="text text-white">FB: Claire Lounge</span>
+                </li>
+                <li>
+                  <span class="text text-white">IG: Claire Lounge</span>
+                </li>
+                <li>
+                  <span class="text text-white">TIKTOK: Claire Lounge</span>
+                </li>
+              </ul>
             </div>
-            
-          </div>
-         
-          <div class="col-md" style="padding-left: 150px">
-             
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-            	<h2 class="ftco-heading-2">Have a Questions?</h2>
-            	<div class="block-23 mb-3">
-                <ul>
-                    <li><span class="icon icon-map-marker"></span><span class="text text-white">890,Sector 62, Gyan Sarovar, GAIL Noida(Delhi/NCR)</span></li>
-                    <li><a href="#"><span class="icon icon-phone"></span><span class="text">+7896541236</span></a></li>
-                    <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@gmail.com</span></a></li>
-                  </ul>
-                </div>
-              </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-12 text-center">
 
-            <p>
-  2023 © Claire Beauty Lounge SJDM </p>
+        <div class="col-md" style="padding-left: 150px"></div>
+        <div class="col-md">
+          <div class="ftco-footer-widget mb-4">
+            <h2 class="ftco-heading-2">Have a Questions?</h2>
+            <div class="block-23 mb-3">
+              <ul>
+                <li>
+                  <span class="icon icon-map-marker"></span
+                  ><span class="text text-white"
+                    >Skyline Drive, San Jose del Monte, Bulacan, Philippines,
+                    3023</span
+                  >
+                </li>
+                <li>
+                  <a href="#"
+                    ><span class="icon icon-phone"></span
+                    ><span class="text">0965 365 6545</span></a
+                  >
+                </li>
+                <li>
+                  <a href="#"
+                    ><span class="icon icon-envelope"></span
+                    ><span class="text">clairelounge2023@gmail.com</span></a
+                  >
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </footer>
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <p>2023 © Claire Beauty Lounge SJDM</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
